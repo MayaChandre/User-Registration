38c19419c6396f7ebc0531e102e6d9467cf29e0c
mayavathi <mayachandre27@gmail.com>
2021-07-11 05:33:42 -0400

    Check Valid Password atleast 1 Capital

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -7,7 +7,8 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Welcome to User-Registration");
 		System.out.println("Enter Password:");
 		Scanner sc = new Scanner(System.in);
